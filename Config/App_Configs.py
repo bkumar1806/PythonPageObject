c655f55df52574610121a921ae1c1a6a7b13b61d
bkumar1806 <kumarbhunesh@gmail.com>
2020-10-18 09:37:54 +0530

    Update App_Configs.py

--- Config/App_Configs.py
+++ Config/App_Configs.py
@@ -1,7 +1,7 @@
 import os
 
 class TestData:
-    CHROME_DRIVER_EXE_PATH = os.getcwd()+ "\\Drivers\\chromedriver.exe"
+    CHROME_DRIVER_EXE_PATH = "s:\\Drivers\\chromedriver.exe"
     FIREFOX_DRIVER_EXE_PATH = os.getcwd()+ "\\Drivers\\geckodriver.exe"
     IE_DRIVER_EXE_PATH = ""
     BASE_URL = "https://pghintranet1.win.ansys.com/tools/ReliabilityMetrics/Account/Login.aspx"
