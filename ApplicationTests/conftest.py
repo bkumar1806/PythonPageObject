c1f4f0028a3bd31875c97fce017ad174d85944a5
bkumar1806 <kumarbhunesh@gmail.com>
2020-10-18 10:10:36 +0530

    Update conftest.py

--- ApplicationTests/conftest.py
+++ ApplicationTests/conftest.py
@@ -7,8 +7,10 @@ from Config.App_Configs import TestData
 
 @pytest.fixture(params = ["chrome"], scope="class")
 def init_driver(request):
-    if request.param == "chrome":        
-        web_driver = webdriver.Chrome(executable_path=TestData.CHROME_DRIVER_EXE_PATH)
+    if request.param == "chrome":
+        chrome_Options = webdriver.ChromeOptions()
+        chrome_Options.binary_location = $(chromebinary)
+        web_driver = webdriver.Chrome(executable_path=TestData.CHROME_DRIVER_EXE_PATH,chrome_options = chrome_Options)
     if request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=TestData.FIREFOX_DRIVER_EXE_PATH)
     request.cls.driver = web_driver
